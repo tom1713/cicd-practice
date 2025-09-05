9c0f740107468c55ea76c33f5f3a83856bf29e42
tom1713 <69420558+tom1713@users.noreply.github.com>
2025-09-05 17:34:59 +0800

    update version

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# 設定版本號，每次想測試重新部署就改這個數字
+VERSION = "v1.0.0"
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello CI/CD"}
+    return {"message": "Hello CI/CD", "version": VERSION}
+
+@app.get("/version")
+def get_version():
+    return {"version": VERSION}
